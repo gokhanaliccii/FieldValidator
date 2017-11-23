d6616837a9f622fc07211cf68ef02107ec8d97bd
Gökhan Alıcı <gokhanaliccii@gmail.com>
2017-11-23 23:06:52 +0200

    Model field test added

--- app/src/test/java/com/gokhanaliccii/fieldvalidator/PasswordAnnotationTest.java
+++ app/src/test/java/com/gokhanaliccii/fieldvalidator/PasswordAnnotationTest.java
@@ -3,6 +3,7 @@ package com.gokhanaliccii.fieldvalidator;
 import com.gokhanaliccii.fieldvalidator.validator.annotation.ValidatePassword;
 import com.gokhanaliccii.fieldvalidator.validator.core.adapter.RuleAdapter;
 import com.gokhanaliccii.fieldvalidator.validator.core.model.BaseRule;
+import com.gokhanaliccii.fieldvalidator.validator.core.model.PasswordRule;
 
 import junit.framework.Assert;
 
@@ -17,16 +18,28 @@ import java.util.List;
 public class PasswordAnnotationTest {
 
     @Test
-    public void shouldad_CreateSingleFromModel() {
+    public void should_CreateSingleFromModel() {
 
         Model testModel = new Model("abc12");
 
         List<BaseRule> rules = new RuleAdapter(testModel).convert();
         Assert.assertTrue(rules.size() == 1);
-
     }
 
-    private static class Model {
+    @Test
+    public void should_AccessToFieldOfModelCorrectly() {
+        final String expectedInput = "abc12";
+        Model testModel = new Model(expectedInput);
+
+        List<BaseRule> rules = new RuleAdapter(testModel).convert();
+        PasswordRule passwordRule = (PasswordRule) rules.get(0);
+
+        String actualInput = passwordRule.getInput();
+
+        Assert.assertEquals(expectedInput,actualInput);
+    }
+
+    public static class Model {
 
         @ValidatePassword(minNumberCount = 1)
         public String password;
